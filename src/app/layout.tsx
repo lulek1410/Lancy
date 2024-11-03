2899099ffc687c546cbb75ab57ee4c4a358b5410
lulek1410 <jan.szewczynski@gmail.com>
2024-11-03 19:57:50 +0100

    LAN-11(feat): Add font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,14 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-
 import AppLayout from "@/components/templates/AppLayout";
+import type { Metadata } from "next";
+import { Overpass } from "next/font/google";
 
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const overpass = Overpass({
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -29,9 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${overpass.className} antialiased w-screen h-screen`}>
         <AppLayout>{children}</AppLayout>
       </body>
     </html>
