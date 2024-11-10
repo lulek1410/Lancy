165531c845ef629b497c775625a247671f29c65f
lulek1410 <jan.szewczynski@gmail.com>
2024-11-10 11:51:23 +0100

    LAN-7(style): Add gradient to header

--- src/components/molecules/App/Header.tsx
+++ src/components/molecules/App/Header.tsx
@@ -1,10 +1,11 @@
+import React from "react";
+
 import Avatar from "@/components/atoms/App/Avatar";
 import Logo from "@/components/atoms/App/Logo";
-import React from "react";
-//bg-gradient-to-t from-[hsl(7,79%,32%)] from-10% to-moccaccino-800 to-30%
+
 const Header = () => {
   return (
-    <header className="flex h-16 items-center justify-between bg-[#3B429F] px-10">
+    <header className="flex h-16 items-center justify-between bg-gradient-to-t from-[hsl(210,89%,26%)] from-10% to-congress-blue-800 to-30% px-10">
       <Logo />
       <Avatar />
     </header>
